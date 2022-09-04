c3b125e99d730d68bcab8b4d314a929b1114be8a
James Elazegui <jameselazegui21@gmail.com>
2022-09-04 23:47:54 +1200

    update

--- js/Corrupt_Coffee_Website/js/script.js
+++ js/Corrupt_Coffee_Website/js/script.js
@@ -15,24 +15,3 @@ window.addEventListener("scroll", reveal);
 
 // To check the scroll position on page load
 reveal();
-
-// menu overlay
-var menuStat = Boolean;
-var hamburgerBtn = document.querySelector(".hamburger");
-var closeBtn = document.querySelector(".close");
-var menuOl = document.querySelector(".menu-overlay");
-
-hamburgerBtn.addEventListener("click", () => {
-   menuOl.classList.add("open");
-   menuStat = true;
-});
-closeBtn.addEventListener("click", () => {
-   menuOl.classList.remove("open");
-   menuStat = false;
-});
-
-onmousedown = function (e) {
-   if (e.target != menuOl) {
-      menuOl.classList.remove("open");
-   }
-};
