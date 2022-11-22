37e6f67241c1b6b4be8da97d6ee559f9c0e90027
James Elazegui <110789359+jamesnpe13@users.noreply.github.com>
2022-11-22 15:13:47 +1300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -5,9 +5,10 @@ var buttons = document.querySelectorAll(".repo-link");
 
 for (var button of buttons) {
    button.addEventListener("click", (event) => {
-      var repoLink = event.target.getAttribute("data-link");
-      window.open(repoLink, "_blank");
+      var repoLink = `${event.target.getAttribute("data-link")}/index.html`;
       console.log(repoLink);
+      window.open(repoLink, "_blank");
+      
 //       location.href = repoLink;
    });
 }
